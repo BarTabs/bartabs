2e9fa4a7d45384b39f8658059e154a3d40e4d981
victorlora <victor@victorlora.net>
2017-02-01 15:06:47 -0500

    Initial commit

---
+++ bartabs-ws/src/main/java/com/bartabs/ws/controller/BarTabsController.java
@@ -0,0 +1,25 @@
+package com.bartabs.ws.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bartabs.ws.model.Shop;
+
+@Controller
+public class BarTabsController {
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public @ResponseBody Shop getShopInJSON(@RequestParam("name") String name) {
+
+		Shop shop = new Shop();
+		shop.setName(name);
+		shop.setStaffName("staff");
+
+		return shop;
+
+	}
+
+}
