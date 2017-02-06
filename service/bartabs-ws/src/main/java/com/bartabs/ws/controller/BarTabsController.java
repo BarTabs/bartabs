84e62f26e36d406be07d9f74ee487d0b4dc692cf
victorlora <victor@victorlora.net>
2017-02-06 16:56:14 -0500

    Set up database jdbc connection

--- service/bartabs-ws/src/main/java/com/bartabs/ws/controller/BarTabsController.java
+++ service/bartabs-ws/src/main/java/com/bartabs/ws/controller/BarTabsController.java
@@ -1,31 +1,23 @@
 package com.bartabs.ws.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bartabs.ws.dao.TestDao;
 import com.bartabs.ws.model.Shop;
 
 @Controller
 public class BarTabsController {
+	@Autowired
+	TestDao dao;
 
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public @ResponseBody Shop getShopInJSON(@RequestParam("name") String name) {
-
-		Shop shop = new Shop();
-		shop.setName(name);
-		shop.setStaffName("Test2");
-
-		Shop shop2 = new Shop();
-		shop2.setName("John");
-		shop2.setStaffName("Goldberg");
-
-		shop.setShop(shop2);
-
-		return shop;
-
+		return dao.getData();
 	}
 
 }
