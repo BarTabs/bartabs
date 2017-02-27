d3f020d9551e686e9277809d205af4f5169fdec4
Jonathan Goldberg <jonraphy@gmail.com>
2017-02-27 15:53:43 -0500

    worked on JSON Table UI

--- ios-application/Bar Tabs/menuViewController.swift
+++ ios-application/Bar Tabs/menuViewController.swift
@@ -11,17 +11,24 @@ import Alamofire
 import SwiftyJSON
 
 class menuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    var TableData : Array<String> = Array <String>()
     
+//    var menu: NSDictionary = NSDictionary()
+    var menu : JSON?
+    //var menu : NSDictionary = NSDictionary()
+
     let url = "http://138.197.87.137:8080/bartabs-server/test"
     
-    @IBOutlet var table: UITableView!
     
+    @IBOutlet weak var tableView: UITableView!
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
         let token = UserDefaults.standard.string(forKey: "token")!
         
@@ -29,13 +36,29 @@ class menuViewController: UIViewController, UITableViewDataSource, UITableViewDe
             "Authorization" : token
         ]
         
+        
+        
         Alamofire.request(url, method: .get, encoding: URLEncoding.default, headers: headers).responseJSON { response in
             if((response.result.value) != nil) {
                 let jsonVar : JSON = JSON(response.result.value ?? "success")
-                let name = String(describing: jsonVar["data"][0]["name"])
-                print(jsonVar["data"])
-                print(jsonVar["data"][0]["name"])
-                self.TableData.append(name)
+                self.menu = jsonVar
+//                self.menu = data["name"]
+//                print(self.menu)
+                
+//                self.menu = NSDictionary(contentsOfFile: String(describing: jsonVar["data"]))!
+//                self.menu = jsonVar["data"]
+//                print(self.menu)
+                
+                //print(self.menu?[0]["name"])
+                
+//                let name = jsonVar["data"][0]["name"]
+//                print(name)
+                //reponseArray = jsonVar as! Array
+                //let name = String(describing: jsonVar["data"][0]["name"])
+                //print(jsonVar["data"])
+                //print(jsonVar["data"][0]["name"])
+                //self.TableData.append(name)
+                self.tableView.reloadData()
             } else {
                 self.createAlert(titleText: "Data Error", messageText: "There was a problem receiving the data")
             }
@@ -48,12 +71,19 @@ class menuViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TableData.count
+//        return menu.count
+//        return self.menu.count
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = TableData[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let category = self.menu?["data"][indexPath.row]["name"] as AnyObject? as? String ?? ""
+        cell.textLabel?.text = category
+//        cell.imageView?.image = UIImage(named: object["image"]!)
+//        cell.otherLabel?.text =  object["otherProperty"]!
+        
         return cell
     }
     
