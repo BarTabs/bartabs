3ce390297241f82d159079b5c7832f218bc6f965
Jonathan Goldberg <jonraphy@gmail.com>
2017-03-22 01:15:42 -0400

    -Added placing order request.

--- ios-application/Bar Tabs/orderViewController.swift
+++ ios-application/Bar Tabs/orderViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import Alamofire
 import SwiftyJSON
 
 class orderViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var item : JSON?
     var items = [JSON]()
+    let url = "http://138.197.87.137:8080/bartabs-server/order/placeorder"
+    let container: UIView = UIView()
+    let loadingView: UIView = UIView()
+    let activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+
     
     @IBOutlet var tableView: UITableView!
     
     @IBOutlet var totalPrice: UILabel!
     
+    
+    @IBAction func pay(_ sender: Any) {
+
+        showActivityIndicatory(uiView: self.view)
+        let token = UserDefaults.standard.string(forKey: "token")!
+        let userID = UserDefaults.standard.string(forKey: "userID")!
+        
+        let headers : HTTPHeaders = [
+            "Authorization" : token
+        ]
+        
+        let parameters: Parameters = [
+            "orderedBy" : userID,
+            "barID" : 4
+        ]
+        
+        URLCache.shared.removeAllCachedResponses()
+        
+        Alamofire.request(url, method: .post, parameters: parameters, encoding: URLEncoding.default, headers: headers).responseJSON { response in
+            if((response.result.value) != nil) {
+                self.hideActivityIndicator(uiView: self.view)
+                self.performSegue(withIdentifier: "paySegue", sender: nil)
+            } else {
+                self.hideActivityIndicator(uiView: self.view)
+                self.createAlert(titleText: "Order Error", messageText: "Order could not be placed")
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,4 +102,50 @@ class orderViewController: UIViewController, UITableViewDataSource, UITableViewD
         self.tableView.separatorStyle = .none
         self.tableView.backgroundColor = UIColor(red: 0.9608, green: 0.9608, blue: 0.8627, alpha: 1.0)
     }
+    
+    //Create alert function
+    func createAlert(titleText: String, messageText: String) {
+        
+        let alert = UIAlertController(title: titleText, message: messageText, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) in alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //Create func for activity indicator to display on screen
+    func showActivityIndicatory(uiView: UIView) {
+        container.frame = uiView.frame
+        container.center = uiView.center
+        container.backgroundColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.5)
+        
+        loadingView.frame = CGRectMake(0, 0, 80, 80)
+        loadingView.center = uiView.center
+        loadingView.backgroundColor = UIColor(red:0.27, green:0.27, blue:0.27, alpha:1.0)
+        loadingView.clipsToBounds = true
+        loadingView.layer.cornerRadius = 10
+        
+        activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        activityIndicator.activityIndicatorViewStyle =
+            UIActivityIndicatorViewStyle.whiteLarge
+        activityIndicator.center = CGPoint(x:loadingView.frame.size.width / 2,
+                                           y:loadingView.frame.size.height / 2);
+        loadingView.addSubview(activityIndicator)
+        container.addSubview(loadingView)
+        uiView.addSubview(container)
+        activityIndicator.startAnimating()
+    }
+    
+    //Create func to hide the activity indicator
+    func hideActivityIndicator(uiView: UIView) {
+        activityIndicator.stopAnimating()
+        container.removeFromSuperview()
+    }
+    
+    //Create func for rectangular graphic container for activity indicator
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+
 }
---
+++ ios-application/Bar Tabs/payViewController.swift
@@ -0,0 +1,26 @@
+//
+//  payViewController.swift
+//  Bar Tabs
+//
+//  Created by Dexstrum on 3/22/17.
+//  Copyright © 2017 muhlenberg. All rights reserved.
+//
+
+import UIKit
+
+class payViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
