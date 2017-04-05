7e4d2390952fe1ecca95cae67af10917d2500d4b
Jonathan Goldberg <jonraphy@gmail.com>
2017-04-05 14:04:45 -0400

    -Added map view to display user current location

---
+++ ios-application/Bar Tabs/mapViewController.swift
@@ -0,0 +1,65 @@
+//
+//  mapViewController.swift
+//  Bar Tabs
+//
+//  Created by Dexstrum on 4/5/17.
+//  Copyright © 2017 muhlenberg. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class mapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+    
+    @IBOutlet var map: MKMapView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    var manager = CLLocationManager()
+    
+    @IBAction func segmentedChanged(_ sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            map.mapType = MKMapType.standard
+            break
+        case 1:
+            map.mapType = MKMapType.satellite
+            break
+        case 2:
+            map.mapType = MKMapType.hybrid
+            break
+        default:
+            break
+        }
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0]
+        let latitude:CLLocationDegrees = userLocation.coordinate.latitude
+        let longitude:CLLocationDegrees = userLocation.coordinate.longitude
+        let latDelta:CLLocationDegrees = 0.01
+        let lonDelta:CLLocationDegrees = 0.01
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+    }
+    
+
+}
