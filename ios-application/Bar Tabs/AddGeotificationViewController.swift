058c225545a974578c8de649fd56dec67e720a80
victorlora <victor@victorlora.net>
2017-04-27 17:00:51 -0400

    Modify view controllers to work correctly with our implementations

--- ios-application/Bar Tabs/AddGeotificationViewController.swift
+++ ios-application/Bar Tabs/AddGeotificationViewController.swift
@@ -22,47 +22,89 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 protocol AddGeotificationsViewControllerDelegate {
-  func addGeotificationViewController(controller: AddGeotificationViewController, didAddCoordinate coordinate: CLLocationCoordinate2D,
-    radius: Double, identifier: String, note: String, eventType: EventType)
+    func addGeotificationViewController(controller: AddGeotificationViewController, didAddCoordinate coordinate: CLLocationCoordinate2D,
+                                        radius: Double, identifier: String, note: String, eventType: EventType)
 }
 
-class AddGeotificationViewController: UITableViewController {
-
-  @IBOutlet var addButton: UIBarButtonItem!
-  @IBOutlet var zoomButton: UIBarButtonItem!
-  @IBOutlet weak var eventTypeSegmentedControl: UISegmentedControl!
-  @IBOutlet weak var radiusTextField: UITextField!
-  @IBOutlet weak var noteTextField: UITextField!
-  @IBOutlet weak var mapView: MKMapView!
-
-  var delegate: AddGeotificationsViewControllerDelegate?
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    navigationItem.rightBarButtonItems = [addButton, zoomButton]
-    addButton.isEnabled = false
-  }
-
-  @IBAction func textFieldEditingChanged(sender: UITextField) {
-    addButton.isEnabled = !radiusTextField.text!.isEmpty && !noteTextField.text!.isEmpty
-  }
-
-  @IBAction func onCancel(sender: AnyObject) {
-    dismiss(animated: true, completion: nil)
-  }
-
-  @IBAction private func onAdd(sender: AnyObject) {
-    let coordinate = mapView.centerCoordinate
-    let radius = Double(radiusTextField.text!) ?? 0
-    let identifier = NSUUID().uuidString
-    let note = noteTextField.text
-    let eventType: EventType = (eventTypeSegmentedControl.selectedSegmentIndex == 0) ? .onEntry : .onExit
-    delegate?.addGeotificationViewController(controller: self, didAddCoordinate: coordinate, radius: radius, identifier: identifier, note: note!, eventType: eventType)
-  }
-
-  @IBAction private func onZoomToCurrentLocation(sender: AnyObject) {
-    mapView.zoomToUserLocation()
-  }
+class AddGeotificationViewController: UITableViewController, MKMapViewDelegate {
+    
+    @IBOutlet var addButton: UIBarButtonItem!
+    @IBOutlet var zoomButton: UIBarButtonItem!
+    @IBOutlet weak var eventTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var radiusTextField: UITextField!
+    @IBOutlet weak var noteTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var delegate: AddGeotificationsViewControllerDelegate?
+    var currentRadius = 100.0
+    var currentOverlay: MKCircle = MKCircle()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItems = [addButton, zoomButton]
+        addButton.isEnabled = false
+        self.mapView.delegate = self
+        self.mapView.addAnnotation(_annotation!)
+        self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+        self.addRadiusOverlay(annotation: _annotation!, radius: 100.0)
+        currentRadius = 100.0
+    }
+    
+    // MARK: Map overlay functions
+    func addRadiusOverlay(annotation: MKPointAnnotation, radius: CLLocationDistance) {
+        currentOverlay = MKCircle(center: annotation.coordinate, radius: radius)
+        self.mapView.add(currentOverlay)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let circleRenderer = MKCircleRenderer(overlay: overlay)
+            circleRenderer.lineWidth = 1.0
+            circleRenderer.strokeColor = .purple
+            circleRenderer.fillColor = UIColor.purple.withAlphaComponent(0.4)
+            return circleRenderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
+    }
+    
+    
+    func removeRadiusOverlay() {
+        guard let overlays = mapView?.overlays else { return }
+        for overlay in overlays {
+            guard let circleOverlay = overlay as? MKCircle else { continue }
+            mapView?.remove(circleOverlay)
+        }
+    }
+    
+    
+    @IBAction func textFieldEditingChanged(sender: UITextField) {
+        addButton.isEnabled = !radiusTextField.text!.isEmpty && !noteTextField.text!.isEmpty
+    }
+    
+    
+    @IBAction func radiusTextDidChange(_ sender: Any) {
+        removeRadiusOverlay()
+        currentRadius = Double(radiusTextField.text!) ?? 100.0
+        addRadiusOverlay(annotation: _annotation!, radius: currentRadius)
+    }
+    
+    @IBAction func onCancel(sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction private func onAdd(sender: AnyObject) {
+        let coordinate = _annotation!.coordinate
+        let radius = Double(radiusTextField.text!) ?? 0
+        let identifier = NSUUID().uuidString
+        let note = noteTextField.text
+        let eventType: EventType = (eventTypeSegmentedControl.selectedSegmentIndex == 0) ? .onEntry : .onExit
+        delegate?.addGeotificationViewController(controller: self, didAddCoordinate: coordinate, radius: radius, identifier: identifier, note: note!, eventType: eventType)
+    }
+    
+    @IBAction private func onZoomToCurrentLocation(sender: AnyObject) {
+        mapView.zoomToUserLocation()
+    }
 }
--- ios-application/Bar Tabs/GeotificationsViewController.swift
+++ ios-application/Bar Tabs/GeotificationsViewController.swift
@@ -25,149 +25,176 @@ import MapKit
 import CoreLocation
 
 struct PreferencesKeys {
-  static let savedItems = "savedItems"
+    static let savedItems = "savedItems"
 }
 
+var _annotation: MKPointAnnotation?
+
+
 class GeotificationsViewController: UIViewController {
-  
-  @IBOutlet weak var mapView: MKMapView!
-  
-  var geotifications: [Geotification] = []
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    loadAllGeotifications()
-  }
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "addGeotification" {
-      let navigationController = segue.destination as! UINavigationController
-      let vc = navigationController.viewControllers.first as! AddGeotificationViewController
-      vc.delegate = self
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var geotifications: [Geotification] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Long press to drop pin
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotationOnLongPress(gesture:)))
+        longPressGesture.minimumPressDuration = 0.5
+        self.mapView.addGestureRecognizer(longPressGesture)
+        
+        loadAllGeotifications()
     }
-  }
-  
-  // MARK: Loading and saving functions
-  func loadAllGeotifications() {
-    geotifications = []
-    guard let savedItems = UserDefaults.standard.array(forKey: PreferencesKeys.savedItems) else { return }
-    for savedItem in savedItems {
-      guard let geotification = NSKeyedUnarchiver.unarchiveObject(with: savedItem as! Data) as? Geotification else { continue }
-      add(geotification: geotification)
+    
+    func addAnnotationOnLongPress(gesture: UILongPressGestureRecognizer) {
+        
+        if _annotation != nil {
+            self.mapView.removeAnnotation(_annotation!)
+        }
+        
+        let point = gesture.location(in: self.mapView)
+        let coordinate = self.mapView.convert(point, toCoordinateFrom: self.mapView)
+        print(coordinate)
+        //Now use this coordinate to add annotation on map.
+        _annotation = MKPointAnnotation()
+        _annotation?.coordinate = coordinate
+        //Set title and subtitle if you want
+        _annotation?.title = "New Bar"
+        _annotation?.subtitle = "Click + to add geofence"
+        self.mapView.addAnnotation(_annotation!)
     }
-  }
-  
-  func saveAllGeotifications() {
-    var items: [Data] = []
-    for geotification in geotifications {
-      let item = NSKeyedArchiver.archivedData(withRootObject: geotification)
-      items.append(item)
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addGeotification" {
+            let navigationController = segue.destination as! UINavigationController
+            let vc = navigationController.viewControllers.first as! AddGeotificationViewController
+            vc.delegate = self
+        }
     }
-    UserDefaults.standard.set(items, forKey: PreferencesKeys.savedItems)
-  }
-  
-  // MARK: Functions that update the model/associated views with geotification changes
-  func add(geotification: Geotification) {
-    geotifications.append(geotification)
-    mapView.addAnnotation(geotification)
-    addRadiusOverlay(forGeotification: geotification)
-    updateGeotificationsCount()
-  }
-  
-  func remove(geotification: Geotification) {
-    if let indexInArray = geotifications.index(of: geotification) {
-      geotifications.remove(at: indexInArray)
+    
+    // MARK: Loading and saving functions
+    func loadAllGeotifications() {
+        geotifications = []
+        guard let savedItems = UserDefaults.standard.array(forKey: PreferencesKeys.savedItems) else { return }
+        for savedItem in savedItems {
+            guard let geotification = NSKeyedUnarchiver.unarchiveObject(with: savedItem as! Data) as? Geotification else { continue }
+            add(geotification: geotification)
+        }
     }
-    mapView.removeAnnotation(geotification)
-    removeRadiusOverlay(forGeotification: geotification)
-    updateGeotificationsCount()
-  }
-  
-  func updateGeotificationsCount() {
-    title = "Geotifications (\(geotifications.count))"
-  }
-  
-  // MARK: Map overlay functions
-  func addRadiusOverlay(forGeotification geotification: Geotification) {
-    mapView?.add(MKCircle(center: geotification.coordinate, radius: geotification.radius))
-  }
-  
-  func removeRadiusOverlay(forGeotification geotification: Geotification) {
-    // Find exactly one overlay which has the same coordinates & radius to remove
-    guard let overlays = mapView?.overlays else { return }
-    for overlay in overlays {
-      guard let circleOverlay = overlay as? MKCircle else { continue }
-      let coord = circleOverlay.coordinate
-      if coord.latitude == geotification.coordinate.latitude && coord.longitude == geotification.coordinate.longitude && circleOverlay.radius == geotification.radius {
-        mapView?.remove(circleOverlay)
-        break
-      }
+    
+    func saveAllGeotifications() {
+        var items: [Data] = []
+        for geotification in geotifications {
+            let item = NSKeyedArchiver.archivedData(withRootObject: geotification)
+            items.append(item)
+        }
+        UserDefaults.standard.set(items, forKey: PreferencesKeys.savedItems)
     }
-  }
-  
-  // MARK: Other mapview functions
-  @IBAction func zoomToCurrentLocation(sender: AnyObject) {
-    mapView.zoomToUserLocation()
-  }
-  
+    
+    // MARK: Functions that update the model/associated views with geotification changes
+    func add(geotification: Geotification) {
+        geotifications.append(geotification)
+        mapView.addAnnotation(geotification)
+        addRadiusOverlay(forGeotification: geotification)
+        updateGeotificationsCount()
+    }
+    
+    func remove(geotification: Geotification) {
+        if let indexInArray = geotifications.index(of: geotification) {
+            geotifications.remove(at: indexInArray)
+        }
+        mapView.removeAnnotation(geotification)
+        removeRadiusOverlay(forGeotification: geotification)
+        updateGeotificationsCount()
+    }
+    
+    func updateGeotificationsCount() {
+        title = "Geotifications (\(geotifications.count))"
+    }
+    
+    // MARK: Map overlay functions
+    func addRadiusOverlay(forGeotification geotification: Geotification) {
+        mapView?.add(MKCircle(center: geotification.coordinate, radius: geotification.radius))
+    }
+    
+    func removeRadiusOverlay(forGeotification geotification: Geotification) {
+        // Find exactly one overlay which has the same coordinates & radius to remove
+        guard let overlays = mapView?.overlays else { return }
+        for overlay in overlays {
+            guard let circleOverlay = overlay as? MKCircle else { continue }
+            let coord = circleOverlay.coordinate
+            if coord.latitude == geotification.coordinate.latitude && coord.longitude == geotification.coordinate.longitude && circleOverlay.radius == geotification.radius {
+                mapView?.remove(circleOverlay)
+                break
+            }
+        }
+    }
+    
+    // MARK: Other mapview functions
+    @IBAction func zoomToCurrentLocation(sender: AnyObject) {
+        mapView.zoomToUserLocation()
+    }
+    
 }
 
 // MARK: AddGeotificationViewControllerDelegate
 extension GeotificationsViewController: AddGeotificationsViewControllerDelegate {
-  
-  func addGeotificationViewController(controller: AddGeotificationViewController, didAddCoordinate coordinate: CLLocationCoordinate2D, radius: Double, identifier: String, note: String, eventType: EventType) {
-    controller.dismiss(animated: true, completion: nil)
-    let geotification = Geotification(coordinate: coordinate, radius: radius, identifier: identifier, note: note, eventType: eventType)
-    add(geotification: geotification)
-    saveAllGeotifications()
-  }
-  
+    
+    func addGeotificationViewController(controller: AddGeotificationViewController, didAddCoordinate coordinate: CLLocationCoordinate2D, radius: Double, identifier: String, note: String, eventType: EventType) {
+        controller.dismiss(animated: true, completion: nil)
+        let geotification = Geotification(coordinate: coordinate, radius: radius, identifier: identifier, note: note, eventType: eventType)
+        add(geotification: geotification)
+        saveAllGeotifications()
+    }
+    
 }
 
 // MARK: - Location Manager Delegate
 extension GeotificationsViewController: CLLocationManagerDelegate {
-  
+    
 }
 
 // MARK: - MapView Delegate
 extension GeotificationsViewController: MKMapViewDelegate {
-  
-  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-    let identifier = "myGeotification"
-    if annotation is Geotification {
-      var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
-      if annotationView == nil {
-        annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-        annotationView?.canShowCallout = true
-        let removeButton = UIButton(type: .custom)
-        removeButton.frame = CGRect(x: 0, y: 0, width: 23, height: 23)
-        removeButton.setImage(UIImage(named: "DeleteGeotification")!, for: .normal)
-        annotationView?.leftCalloutAccessoryView = removeButton
-      } else {
-        annotationView?.annotation = annotation
-      }
-      return annotationView
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "myGeotification"
+        if annotation is Geotification {
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+                let removeButton = UIButton(type: .custom)
+                removeButton.frame = CGRect(x: 0, y: 0, width: 23, height: 23)
+                removeButton.setImage(UIImage(named: "DeleteGeotification")!, for: .normal)
+                annotationView?.leftCalloutAccessoryView = removeButton
+            } else {
+                annotationView?.annotation = annotation
+            }
+            return annotationView
+        }
+        return nil
     }
-    return nil
-  }
-  
-  
-  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-    if overlay is MKCircle {
-      let circleRenderer = MKCircleRenderer(overlay: overlay)
-      circleRenderer.lineWidth = 1.0
-      circleRenderer.strokeColor = .purple
-      circleRenderer.fillColor = UIColor.purple.withAlphaComponent(0.4)
-      return circleRenderer
+    
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let circleRenderer = MKCircleRenderer(overlay: overlay)
+            circleRenderer.lineWidth = 1.0
+            circleRenderer.strokeColor = .purple
+            circleRenderer.fillColor = UIColor.purple.withAlphaComponent(0.4)
+            return circleRenderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
     }
-    return MKOverlayRenderer(overlay: overlay)
-  }
-  
-  func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-    // Delete geotification
-    let geotification = view.annotation as! Geotification
-    remove(geotification: geotification)
-    saveAllGeotifications()
-  }
-  
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        // Delete geotification
+        let geotification = view.annotation as! Geotification
+        remove(geotification: geotification)
+        saveAllGeotifications()
+    }
+    
 }
